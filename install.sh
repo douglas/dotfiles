2c066bdef3452e312d199ff93de5820f7c4bb89c
Douglas Soares de Andrade <contato@douglasandrade.com>
2024-10-31 23:16:22 -0400

    Add ghostty configuration

--- install.sh
+++ install.sh
@@ -7,5 +7,11 @@ stow bin
 stow git
 stow zsh
 
+if [[ $OPERATINGSYSTEM == 'macos' ]]; then
+  stow ghostty-macos
+else
+  stow ghostty-linux
+fi
+
 # Install git-cob in /usr/local/bin so git can use it
 sudo ln -s $HOME/.bin/git-cob /usr/local/bin/git-cob
