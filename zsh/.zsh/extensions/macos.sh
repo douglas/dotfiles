cdd2d9d27d63cf04dd33703fd8e0012ce6614c07
Douglas Soares de Andrade <contato@douglasandrade.com>
2024-11-29 19:13:00 -0300

    Disable asdf in favour of mise

--- zsh/.zsh/extensions/macos.sh
+++ zsh/.zsh/extensions/macos.sh
@@ -15,9 +15,9 @@ export HOMEBREW_NO_ANALYTICS=1
 [[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
 
 ##
-## ADSF
+## Mise
 ##
-source /opt/homebrew/opt/asdf/libexec/asdf.sh
+eval "$(mise activate zsh)"
 
 ##
 ## Rubymine
