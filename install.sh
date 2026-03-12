23a1badac81b240fa285c49d2eb70399e770df49
Douglas Soares de Andrade <contato@douglasandrade.com>
2026-03-12 16:03:09 -0700

    Use --adopt in stow and restore repo versions after install

--- install.sh
+++ install.sh
@@ -6,25 +6,28 @@
 ##   PROFILE=laptop ./install.sh     # Override profile on Linux
 ##
 
-stow -d ~/.public_dotfiles -t ~ stow
-stow -d ~/.public_dotfiles -t ~ bin
-stow -d ~/.public_dotfiles -t ~ git
-stow -d ~/.public_dotfiles -t ~ zsh
-stow -d ~/.public_dotfiles -t ~ config
+stow --adopt -d ~/.public_dotfiles -t ~ stow
+stow --adopt -d ~/.public_dotfiles -t ~ bin
+stow --adopt -d ~/.public_dotfiles -t ~ git
+stow --adopt -d ~/.public_dotfiles -t ~ zsh
+stow --adopt -d ~/.public_dotfiles -t ~ config
 
 if [[ $OSTYPE == darwin* ]]; then
-  stow -d ~/.public_dotfiles -t ~ config-macos
+  stow --adopt -d ~/.public_dotfiles -t ~ config-macos
 else
   PROFILE="${PROFILE:-$(hostnamectl chassis 2>/dev/null || echo desktop)}"
-  stow -d ~/.public_dotfiles -t ~ config-linux
+  stow --adopt -d ~/.public_dotfiles -t ~ config-linux
 
   if command -v omarchy &>/dev/null; then
-    stow -d ~/.public_dotfiles -t ~ config-omarchy
-    stow -d ~/.public_dotfiles -t ~ "config-omarchy-${PROFILE}"
+    stow --adopt -d ~/.public_dotfiles -t ~ config-omarchy
+    stow --adopt -d ~/.public_dotfiles -t ~ "config-omarchy-${PROFILE}"
   elif [[ -f /etc/os-release ]] && source /etc/os-release && [[ "$ID" == "fedora" ]]; then
-    stow -d ~/.public_dotfiles -t ~ "config-fedora-${PROFILE}"
+    stow --adopt -d ~/.public_dotfiles -t ~ "config-fedora-${PROFILE}"
   fi
 fi
+
+# Restore repo versions after adopt (adopt moves target files into the package)
+git -C ~/.public_dotfiles checkout -- . ':!install.sh'
 
 # Install git-cob in /usr/local/bin so git can use it
 git_cob="/usr/local/bin/git-cob"
